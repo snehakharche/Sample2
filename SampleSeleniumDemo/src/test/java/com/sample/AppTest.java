2aab8fd2ee8f477d18ced6d8dc2d5bb80e3f025f
Sneha <snehakharche7@gmail.com>
2019-08-29 16:16:51 +0530

    Third commit

--- SampleSeleniumDemo/src/test/java/com/sample/AppTest.java
+++ SampleSeleniumDemo/src/test/java/com/sample/AppTest.java
@@ -18,7 +18,7 @@ public class AppTest
     public AppTest( String testName )
     {
         super( testName );
-        System.out.println("Hello FitNesse...");
+        System.out.println("Hello FitNesse...Welcome...");
     }
 
     /**
